92f10b25e7ff053db0b7028824510f315340dd75
scriptsinfra <>
2022-03-12 04:17:06 -0500

    Improved: Updated formatting.

--- syncthing_upgrade.sh
+++ syncthing_upgrade.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-########################################################################################################################################################################
+##############################################################
 
-#Script Name: syncthing_upgrader.sh
+#Script Name: syncthing_upgrade.sh
 
 #Description: This script will automatically upgrade syncthing
 
-########################################################################################################################################################################
+##############################################################
 
 
 # Gracefully stop the syncthing service
 systemctl stop syncthing@syncthing.service
 
-# :
+cd /home/syncthing
+curl -s https://api.github.com/repos/syncthing/syncthing/releases/latest | grep browser_download_url | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
+tar xvf syncthing-linux-amd64*.tar.gz
