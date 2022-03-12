49b531b1617b44a9d913b5fadfdbdd7665bc3005
scriptsinfra <>
2022-03-12 05:35:48 -0500

    New: Added functionality to define email address in file.

--- yum_system_updates.sh
+++ yum_system_updates.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-########################################################################################################################################################################
+###########################################################################################
 
 #Script Name: yum_system_updates.sh
 
 #Description: This script updates yum packages and sends the updated package list to email.
 
-########################################################################################################################################################################
+###########################################################################################
 
-#Define update log
+
+#Define variables
+email=$(cat /git/Scripts/email.txt)
 log=/var/log/yum_update_$(date +\%m.\%d.\%Y).txt
 
 #Run system update
 yum upgrade -y > $log
 
 #Send system update log to email
-cat "$log" | mailx -s "cloud1 System Update Status for $(date +\%m.\%d.\%Y)" andrewbatchelor5@gmail.com
+cat "$log" | mailx -s "cloud1 Yum Updates for $(date +\%m.\%d.\%Y)" "$email"
