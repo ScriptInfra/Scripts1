0cd8ee6e31a8edeebbeafa3f6ebd562a60be91dc
scriptsinfra <>
2022-03-12 03:26:19 -0500

    Improved: Uplifted formatting.

--- surface_go_network_fix.sh
+++ surface_go_network_fix.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/sh
 
 
-######################################################## 
-
+######################################################################################## 
 
 #Script Name: surface_go_network_fix.sh
 
 #Description: This script can be leveraged to revert to the functional wireless firmware
 
+########################################################################################
+
  
-######################################################## 
 
 #Revert from backup
 sudo cp /usr/lib/firmware/ath10k/QCA6174/hw2.1/board.bin_working_backup /usr/lib/firmware/ath10k/QCA6174/hw2.1/board.bin 
