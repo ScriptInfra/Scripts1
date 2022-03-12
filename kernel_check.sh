e1a4c94afe3b6d1150cd5d30f81283007f88c6bd
scriptsinfra <>
2022-03-12 03:57:07 -0500

    Improved: Enhanced formatting.

--- kernel_check.sh
+++ kernel_check.sh
@@ -1,15 +1,17 @@
-#!/bin/sh -x
+#!/bin/sh
 
-########################################################################################################################################################################
+####################################################################################
 
 #Script Name: kernel_check.sh
 
 #Description: This script will check if the system is on the latest kernel available
 
-########################################################################################################################################################################
+####################################################################################
+
 
 # Define variables
 hostname=$(hostname)
+
 rm /var/log/kernel_check_log_$(date +\%m.\%d.\%Y).txt
 touch /var/log/kernel_check_log_$(date +\%m.\%d.\%Y).txt
 log=/var/log/kernel_check_log_$(date +\%m.\%d.\%Y).txt
@@ -23,6 +25,8 @@ if [ "$latest_kernel_version" -gt "$current_kernel_version" ]; then
 		echo "$hostname is not running on the latest kernel version. Please reboot the server to pick up the latest kernel."
 		cat "$log" | mailx -s "$hostname Kernel Check" andrewbatchelor5@gmail.com
 	else
-		echo "$hostname is running on the latest kernel version."
+                echo ""
+                echo "$hostname is running on the latest kernel version."
+		echo ""
 	exit
 fi
