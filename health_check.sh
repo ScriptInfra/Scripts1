a98a37868f4e82a2bf7a500e18d287735742c4b9
scriptsinfra <>
2022-04-22 03:14:22 -0400

    Improved: Reworked formatting. [health_check.sh]

--- health_check.sh
+++ health_check.sh
@@ -18,7 +18,8 @@ clear
 
 email=$(cat /git/Scripts/email.txt)
 
-#Confirm user is running as root
+# Confirm user is running as root
+
 if [ `whoami` != 'root' ]
         then
           echo "You must be root to run this script. This script will now exit."
@@ -26,17 +27,15 @@ if [ `whoami` != 'root' ]
 fi
 
 
-#define and cleanup health check log
-rm /var/log/health_check_logs/health_check_log_$(date +\%m.\%d.\%Y).txt
-touch /var/log/health_check_logs/health_check_log_$(date +\%m.\%d.\%Y).txt
-log=/var/log/health_check_logs/health_check_log_$(date +\%m.\%d.\%Y).txt
 
+# Clear screen for consumption
 
-#Clear screen for consumption
 clear
 
 
-#Prompt that health check script is running
+
+# Prompt that health check script is running
+
 echo | tee -a "$log"
 echo "####################################" | tee -a "$log"
 echo | tee -a "$log"
@@ -48,6 +47,7 @@ echo "####################################" | tee -a "$log"
 #Prompt user that script is checking Syncthing service status
 echo | tee -a "$log"
 echo "Checking Syncthing service status..." | tee -a "$log"
+
 
 
 #Check if Syncthing service is running
@@ -148,7 +148,9 @@ echo | tee -a "$log"
 echo "Checking Firewall service status..." | tee -a "$log"
 
 
-#Check if Firewall service is running
+
+# Check if Firewall service is running
+
 firewall_status=$(systemctl status firewalld | grep Active)
 firewall_status_logic=$(systemctl status firewalld | grep running | wc -l)
 
@@ -172,14 +174,17 @@ fi
 
 
 
-#Prompt user that script is checking cpu temp
+# Prompt user that script is checking cpu temp
+
 echo "------------------------------------------------------------" | tee -a "$log"
 echo | tee -a "$log"
 echo "Checking CPU temp..." | tee -a "$log"
 echo | tee -a "$log"
 
 
-#Check CPU temp
+
+# Check CPU temp
+
 cpu_temp=$(sensors | grep Package | awk '{print $4}' | cut -c 2-3)
 
 if [ $cpu_temp -lt 80 ]; then
