fd4f08ec6c182b7ba3ccb2869f03baf250be38c5
scriptsinfra <>
2022-04-22 02:46:28 -0400

    Improved: Removed unnecessary logging. [kernel_check.sh]

--- kernel_check.sh
+++ kernel_check.sh
@@ -10,24 +10,23 @@
 
 
 # Define variables
+
 hostname=$(hostname)
 email=$(cat /git/Scripts/email.txt)
 
-
-touch /var/log/kernel_check_log_$(date +\%m.\%d.\%Y).txt
-log=/var/log/kernel_check_log_$(date +\%m.\%d.\%Y).txt
 
 latest_kernel_version=$(rpm -q --last kernel | perl -pe 's/^kernel-(\S+).*/$1/' | head -n1 | cut -c 1-11 | tr -d - | tr -d .)
 current_kernel_version=$(uname -r | cut -c 1-11 | tr -d - | tr -d .)
 
 # Check if server is running on the latest kernel available
-if [ "$latest_kernel_version" -gt "$current_kernel_version" ]; then
-		echo "$hostname is not running on the latest kernel version. Please reboot the server to pick up the latest kernel." > "$log"
-		echo "$hostname is not running on the latest kernel version. Please reboot the server to pick up the latest kernel."
-		cat "$log" | mailx -s "$hostname Kernel Check" "$email"
-	else
-                echo ""
-                echo "$hostname is running on the latest kernel version."
-		echo ""
+
+if [ "$latest_kernel_version" -gt "$current_kernel_version" ]
+then
+	echo "$hostname is not running on the latest kernel version. Please reboot the server to pick up the latest kernel."
+	echo "$hostname is not running on the latest kernel version. Please reboot the server to pick up the latest kernel." | mailx -s "$hostname Kernel Check" "$email"
+ else
+	echo ""
+	echo "$hostname is running on the latest kernel version."
+	echo ""
 	exit
 fi
